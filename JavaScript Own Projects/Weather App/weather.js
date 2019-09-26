17d51c9658d7331e66d971b9e726865f073518db
Malina De Jonge <Malina.ScrollingIn@gmail.com>
2019-09-26 12:14:39 +0200

    making a weather app

---
+++ JavaScript Own Projects/Weather App/weather.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector(
+        '.temperature-description'
+        );
+    let temperatureDegree = document.querySelector(
+        '.temperature-degree'
+        );
+    let locationTimezone = document.querySelector(
+        '.location-timezone'
+        );
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            // const proxy = 'https://cors-anywhere.herokuapp.com/'
+            const api = `https://api.darksky.net/forecast/9b3cd144cf97dfa563d57c5730074b1f/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    const {temperature, summary, icon} = data.currently;
+                    //Set DOM Elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                    //Set icon
+                    setIcons(icon, document.querySelector('.icon'));
+                });
+        }); 
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, skycons[currentIcon]);
+    }
+});
